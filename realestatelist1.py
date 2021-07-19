57de6871b46e9890d51f97280ab496d1d73efd2b
Eva Wang <71245111+EvaWang2020@users.noreply.github.com>
2021-07-18 18:32:48 -0700

    Add files via upload

---
+++ realestatelist1.py
@@ -0,0 +1,88 @@
+import json
+import os.path
+from datetime import date
+
+import scrapy
+from ratelimiter import RateLimiter
+from scrapy import signals
+from scrapy.crawler import CrawlerProcess
+from scrapy.signalmanager import dispatcher
+
+# this mean to call 5 pages per 15 seconds . If scraping too fast, it could be blocked. Also, some lists might be skipped when scrapping too fast
+rate_limiter = RateLimiter(max_calls=5, period=15)
+results = []
+date = date.today()
+
+# you can customize your file location
+path = "C://Documents/realestatelist_"
+file_name = str(date) + "-1.txt"
+output_file_path = base_path + file_name
+
+if os.path.isfile(file_path):
+    os.remove(file_path)
+    file_object = open(file_path, "w+")
+    file_object.close()
+else:
+    pass
+
+
+def strip_text(element):
+    if element:
+        return element.strip()
+    return None
+
+
+# create a class inherited from another class scrapy.Spider
+class BlogSpider(scrapy.Spider):
+    name = 'blogspider'
+
+    def __init__(self, city_region):
+        # self.name = name
+        # this is the starting URL
+        self.start_urls = [f'https://www.rew.ca/properties/areas/{city_region}/page/1']
+
+    def parse(self, response):
+        # print(response.text)
+        # Reponse is the scraped result
+        for title in response.css('.displaypanel-body'):
+            yield {
+                # depending on what you want to collect, the detail is different
+                'Price': title.css('.displaypanel-title::text').get().strip(),
+                'Structure & size': title.css('.displaypanel-section.clearfix').css('.l-pipedlist').xpath(
+                    'li/text()').getall(),
+                'Address': title.css('.displaypanel-section::text').get().strip(),
+                'City': title.css('.l-pipedlist.displaypanel-info').xpath('li/text()').getall(),
+                'list_Type': strip_text(title.css('.clearfix.hidden-xs').css('.displaypanel-info::text').get())
+            }  # yield is used to return the result
+
+        # response.css('a[rel="next"]') is a value
+        for next_page in response.css('a[rel="next"]'):
+            with rate_limiter:
+                # this is to ask to go through the pages
+                yield response.follow(next_page, self.parse)
+
+
+# below is a function to call after spider find content
+def crawler_results(signal, sender, item, response, spider):
+    results.append(item)  # item is the scraped result from yield
+
+
+dispatcher.connect(crawler_results, signal=signals.item_passed)
+process = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'
+
+})  # A user agent is any software, acting on behalf of a user, which "retrieves, renders and facilitates end-user interaction with Web content. Sometimes you need to change it to avoid banning from your scrapying websites
+
+process.crawl(BlogSpider, 'vancouver-bc')
+process.crawl(BlogSpider, 'north-vancouver-bc')
+process.crawl(BlogSpider, 'richmond-bc')
+process.crawl(BlogSpider, 'langley-bc')
+
+process.start()
+
+json_result = {
+    'listings': results
+}
+# to create called sample .
+with open(file_path, 'w+') as outfile:
+    outfile.write(json.dumps(json_result) + '\n')
